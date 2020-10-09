3e63d87bd5db3c178cfad4dff85e74f7a9ac24cd
sidorevichAlex <sidorevich19771737@gmail.com>
2020-10-09 14:08:42 +0300

    Load static

--- TEST_FOR_UP/settings.py
+++ TEST_FOR_UP/settings.py
@@ -123,13 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "staticfiles", "media")
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 
