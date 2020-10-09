3e68ade75659450ba76a8d8cfbadbd5ab7f86319
sidorevichAlex <sidorevich19771737@gmail.com>
2020-10-09 10:45:14 +0300

    Load static

--- TEST_FOR_UP/settings.py
+++ TEST_FOR_UP/settings.py
@@ -126,7 +126,8 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
